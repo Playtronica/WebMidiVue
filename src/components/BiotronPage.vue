b59be885b4045cf1837336d65de10502c47432eb
shadowik <zahar.mark@bk.ru>
2023-09-27 15:45:21 +0400

    Get item

--- src/components/BiotronPage.vue
+++ src/components/BiotronPage.vue
@@ -5,21 +5,28 @@
     <template v-slot:objects>
       <SliderCommand command-label="Plant Bpm" :max-value="1000"
                      :default-value="this.commands_data.plantBpm.value"
-                     @change-value="(n) => this.commands_data.plantBpm.value = n"
-      />
-      <SliderCommand command-label="Note Off Percent" :max-value="100" :default-value="100"/>
-      <SliderCommand command-label="Plant Bpm" :max-value="30" :default-value="4"/>
+                     @change-value="(n) => this.commands_data.plantBpm.value = n"/>
+      <SliderCommand command-label="Note Off Percent" :max-value="100"
+                     :default-value="this.commands_data.noteOffPercent.value"
+                     @change-value="(n) => this.commands_data.noteOffPercent.value = n"/>
+      <SliderCommand command-label="Light Bpm" :max-value="30"
+                     :default-value="this.commands_data.lightBpm.value"
+                     @change-value="(n) => this.commands_data.plantBpm.value = n"/>
     </template>
   </GroupOfCommands>
   <GroupOfCommands name-of-group="Sensitivity (fib)">
     <template v-slot:objects>
-      <SliderCommand command-label="Note Distance" :max-value="100" :default-value="50"/>
-      <SliderCommand command-label="First Value" :max-value="100" :default-value="10"/>
+      <SliderCommand command-label="Note Distance" :max-value="100"
+                     :default-value="this.commands_data.noteDistance.value"
+                     @change-value="(n) => this.commands_data.noteDistance.value = n"/>
+      <SliderCommand command-label="First Value" :max-value="100"
+                     :default-value="this.commands_data.firstValue.value"
+                     @change-value="(n) => this.commands_data.firstValue.value = n"/>
     </template>
   </GroupOfCommands>
   <GroupOfCommands name-of-group="Smoothness">
     <template v-slot:objects>
-      <SliderCommand  :max-value="99"/>
+      <SliderCommand  :max-value="99" />
     </template>
   </GroupOfCommands>
   <GroupOfCommands name-of-group="Scale">
@@ -33,7 +40,6 @@
       <SliderCommandWithDisable command-label="Light Velocity" :max-value="127" :default-value="127"/>
     </template>
   </GroupOfCommands>
-  <button @click="console.log(this.commands_data.plantBpm.value)"></button>
 </template>
 
 <script>
@@ -50,10 +56,31 @@ export default {
       scales: ["Major", "Minor", "Chrom", "Dorian", "Mixolydian",
         "Lydian", "Wholetone", "Minblues", "Minpen",
         "Majpen", "Diminished"],
+
       commands_data: {
         "plantBpm": {
-          "index": [0],
+          "index": 0,
           "value": 60
+        },
+        "lightBpm": {
+          "index": 1,
+          "value": 4
+        },
+        "noteOffPercent": {
+          "index": 2,
+          "value": 100
+        },
+        "noteDistance": {
+          "index": 2,
+          "value": 100
+        },
+        "firstValue": {
+          "index": 2,
+          "value": 100
+        },
+        "smoothness": {
+          "index": 2,
+          "value": 100
         }
       }
     }
