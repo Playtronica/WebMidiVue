cd788d91e056f7b90b6c0e5a6ea6583eaa253446
shadowik <zahar.mark@bk.ru>
2025-03-03 18:39:14 +0400

    Change notes numbers

--- src/components/PlaytronPage/PlaytronIDB.js
+++ src/components/PlaytronPage/PlaytronIDB.js
@@ -2,22 +2,22 @@ import {Db} from "@/assets/js/PresetsIDB";
 import {SysExCommand} from "@/assets/js/SysExCommand";
 
 const pads_name = [
-    { "id": 0, "name": "C3", "note": 36 },
-    { "id": 1, "name": "C#3", "note": 37 },
-    { "id": 2, "name": "D3", "note": 38 },
-    { "id": 3, "name": "D#3", "note": 39 },
-    { "id": 4, "name": "E3", "note": 40 },
-    { "id": 5, "name": "F3", "note": 41 },
-    { "id": 6, "name": "F#3", "note": 42 },
-    { "id": 7, "name": "G3", "note": 43 },
-    { "id": 8, "name": "G#3", "note": 44 },
-    { "id": 9, "name": "A3", "note": 45 },
-    { "id": 10, "name": "A#3", "note": 46 },
-    { "id": 11, "name": "B3", "note": 47 },
-    { "id": 12, "name": "C4", "note": 48 },
-    { "id": 13, "name": "C#4", "note": 49 },
-    { "id": 14, "name": "D4", "note": 50 },
-    { "id": 15, "name": "D#4", "note": 51 }
+    { "id": 0, "name": "C3", "note": 60 },
+    { "id": 1, "name": "C#3", "note": 61 },
+    { "id": 2, "name": "D3", "note": 62 },
+    { "id": 3, "name": "D#3", "note": 63 },
+    { "id": 4, "name": "E3", "note": 64 },
+    { "id": 5, "name": "F3", "note": 65 },
+    { "id": 6, "name": "F#3", "note": 66 },
+    { "id": 7, "name": "G3", "note": 67 },
+    { "id": 8, "name": "G#3", "note": 68 },
+    { "id": 9, "name": "A3", "note": 69 },
+    { "id": 10, "name": "A#3", "note": 70 },
+    { "id": 11, "name": "B3", "note": 71 },
+    { "id": 12, "name": "C4", "note": 72 },
+    { "id": 13, "name": "C#4", "note": 73 },
+    { "id": 14, "name": "D4", "note": 74 },
+    { "id": 15, "name": "D#4", "note": 75 }
 ]
 
 
